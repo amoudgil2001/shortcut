2ad06b331f8e5ca8fc28adc1812c11b1cffd67e7
amoudgil2001 <110563729+amoudgil2001@users.noreply.github.com>
2022-11-22 17:13:45 +0530

    Create code.sol

---
+++ code.sol
@@ -0,0 +1,99 @@
+LOTTERY
+pragma solidity ^0.8.11;
+
+contract Lottery {
+    address public owner;
+    address payable[] public players;
+    uint public lotteryId;
+    mapping (uint => address payable) public lotteryHistory;
+
+    constructor() {
+        owner = msg.sender;
+        lotteryId = 1;
+    }
+
+    function getWinnerByLottery(uint lottery) public view returns (address payable) {
+        return lotteryHistory[lottery];
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function getPlayers() public view returns (address payable[] memory) {
+        return players;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether);
+
+        // address of player entering lottery
+        players.push(payable(msg.sender));
+    }
+
+    function getRandomNumber() public view returns (uint) {
+        return uint(keccak256(abi.encodePacked(owner, block.timestamp)));
+    }
+
+    function pickWinner() public onlyowner {
+        uint index = getRandomNumber() % players.length;
+        players[index].transfer(address(this).balance);
+
+        lotteryHistory[lotteryId] = players[index];
+        lotteryId++;
+        
+
+        // reset the state of the contract
+        players = new address payable[](0);
+    }
+
+    modifier onlyowner() {
+      require(msg.sender == owner);
+      _;
+    }
+}
+
+
+VOTING
+pragma solidity 0.5.16;
+
+contract Election {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(address => bool) public voters;
+    mapping(uint => Candidate) public candidates;
+    uint public candidatesCount;
+    uint256 public deadline;
+
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    constructor (uint256 _deadline) public {
+        deadline=_deadline;
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        require(now<deadline);
+        require(!voters[msg.sender]);
+
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        voters[msg.sender] = true;
+
+        candidates[_candidateId].voteCount ++;
+
+        emit votedEvent(_candidateId);
+    }
+}
